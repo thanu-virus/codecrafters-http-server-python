a36d9b1398de1e71a655b3ccd6589a49b8beee29
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-20 11:00:09 +0530

    test

--- app/main.py
+++ app/main.py
@@ -5,8 +5,11 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    connection , address = server_socket.accept(1024) # wait for client
+    if address:
+        print(f"connection succeded with {address}")
+    data = 200
+    conection.sendall(data)
 
 if __name__ == "__main__":
     main()
