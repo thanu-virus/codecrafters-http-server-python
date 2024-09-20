b5c8926eb3abf9e23b08067ecd9da98eb2e378e9
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-21 04:01:44 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
     string:list[str] = request_data[0].split("/")
     echo_string=f"/echo/{string[2]}"
     spit_string=echo_string.split(" ")[1]
-    if request_data[0].split(" ")[1] == spit_string_string:
+    if request_data[0].split(" ")[1] == spit_string:
         response: bytes = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{len(spit_string)}\r\n\r\n{spit_string}\r\n".encode()
     elif request_data[0].split(" ")[1] !="/":
         response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
