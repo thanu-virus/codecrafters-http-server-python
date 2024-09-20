8be0aa315cfc1307128995cdbc09e2b0238a2ff3
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-20 11:48:45 +0530

    test

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
     server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 if __name__ == "__main__":
     main()
