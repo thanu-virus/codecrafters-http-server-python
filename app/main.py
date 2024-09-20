28fe76a28d4255fcd7fa41d1697aa3df193e270b
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-20 11:06:48 +0530

    test

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 import socket  # noqa: F401
-
-
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200  Ok \r\n\r\n") # wait for client
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200  OK\r\n\r\n") # wait for client
 
 if __name__ == "__main__":
     main()
