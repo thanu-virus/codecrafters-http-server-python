1701b6f5d224a0c8a8dd9a2d6c9699a9b12c908f
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-23 17:11:54 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 import socket
 import threading
+import os
 
+# Function to handle client requests
 def handle_client(client_socket: socket.socket):
     data: str = client_socket.recv(1024).decode()
-    
+
     if not data:
         client_socket.close()
         return
 
     request_data: list[str] = data.split("\r\n")
     path_parts: list[str] = request_data[0].split(" ")[1].split("/")
-    
+
     # Handle /echo/{something}
     if len(path_parts) > 2 and path_parts[1] == "echo":
         string_to_echo = path_parts[2]  # The part after /echo/
@@ -36,39 +38,7 @@ def handle_client(client_socket: socket.socket):
                 f"Content-Length: {len(response_body)}\r\n\r\n"
                 f"{response_body}\r\n"
             ).encode()
-    elif len(path_parts) > 1 and path_parts[1] == "files":
-        string_to_echo = path_parts[2]  # The part after /echo/
-        response_body = string_to_echo
-        response: bytes = (
-                f"HTTP/1.1 200 OK\r\n"
-                f"Content-Type: application/octet-stream\r\n"
-                f"Content-Length: {len(response_body)*2+4}\r\n\r\n"
-                f"{response_body}\r\n"
-            ).encode()
 
-    # Handle any other request path
-    elif path_parts[1] != "":
-        response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
-
-    # Default response (for root path `/`)
-    else:
-        response: bytes = "HTTP/1.1 200 OK\r\n\r\n".encode()
-
-    client_socket.send(response)
-    client_socket.close()
-
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-
-    print("Server is running and waiting for connections...")
-
-    while True:
-        client_socket, addr = server_socket.accept()
-        print(f"Accepted connection from {addr}")
-
-        # Create a new thread to handle the client
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
-        client_handler.start()
-
-if __name__ == "__main__":
-    main()
+    # Handle /files/{filename} to serve file content
+    elif len(path_parts) > 2 and path_parts[1] == "files":
+        file_path = "/".join(path_parts[2:])  # Get the file
