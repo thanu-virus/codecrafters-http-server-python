24fc208dec2ad48592afaf8378da677f12c46bd9
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-26 10:39:34 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ def main():
                         if " gzip" in codType2:
                             response = f"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}".encode()
                         else:
-                            exit
+                            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}".encode()
                     else:
                         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}".encode()
                 
